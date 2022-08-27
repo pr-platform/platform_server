44c17cdce4d2b3db7f535a038d5c3270d5526f4e
supervueman <supervueman@gmail.com>
2022-08-27 17:33:01 +0400

    Add user module starter

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './user/user.model';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      ],
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'pr_platform',
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
